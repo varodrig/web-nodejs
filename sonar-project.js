a99c85f2aff45a2c5d938ff1dc683ee7321ff343
Gerald Nunn <gnunn1@users.noreply.github.com>
2023-02-10 16:06:18 -0800

    Update to use app directory

--- sonar-project.js
+++ sonar-project.js
@@ -6,10 +6,10 @@ sonarqubeScanner(
         //serverUrl:  'http://sonarqube.dev-tools:9000',
         //serverUrl:  'http://localhost:9000',
         options : {
-            'sonar.sources':  'src',
-            'sonar.tests':  'src',
+            'sonar.sources':  'app',
+            'sonar.tests':  'app',
             'sonar.inclusions'  :  '**', // Entry point of your code
-            'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+            'sonar.test.inclusions':  'app/**/*.spec.js,app/**/*.spec.jsx,app/**/*.test.js,app/**/*.test.jsx',
             'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
             'sonar.dependencyCheck.jsonReportPath': 'reports/dependency-check/dependency-check-report.json',
             'sonar.dependencyCheck.htmlReportPath': 'reports/dependency-check/dependency-check-report.html'
